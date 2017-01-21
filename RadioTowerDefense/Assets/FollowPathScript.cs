56f2b8640c65dc1bdbf827c0a81c13f2447a1462
LemonsOnFire <lemonsonfirepro@gmail.com>
2017-01-21 16:51:00 -0500

    assets added

---
+++ RadioTowerDefense/Assets/FollowPathScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathScript : MonoBehaviour {
+    private Vector3 targetVector3;
+    private int speed = 3;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //targetVector3 = iTween.Vector3Update(current, tartget, speed);
+        iTween.MoveUpdate(this.gameObject,iTween.Hash("time",speed,"orienttopath",true,"position",targetVector3));
+	}
+}
